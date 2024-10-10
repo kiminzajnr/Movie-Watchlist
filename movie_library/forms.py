a91a3e4191f1584f153d817297cd79ba22429fe4
kiminzajnr <kiminzaeric@gmail.com>
2024-10-10 13:17:21 +0300

    Add user authentication

--- movie_library/forms.py
+++ movie_library/forms.py
@@ -1,7 +1,7 @@
 from typing import Any
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, StringField, SubmitField, TextAreaField, URLField
-from wtforms.validators import InputRequired, NumberRange
+from wtforms import IntegerField, StringField, SubmitField, TextAreaField, URLField, PasswordField
+from wtforms.validators import InputRequired, NumberRange, Email, EqualTo, Length
 
 
 class MovieForm(FlaskForm):
@@ -41,3 +41,35 @@ class ExendedMovieForm(MovieForm):
     video_link = URLField("Video link")
 
     submit = SubmitField("Submit")
+
+
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[InputRequired(), Email()])
+    password = PasswordField(
+        "password",
+        validators=[
+            InputRequired(),
+            Length(
+                min=4,
+                max=20,
+                message="Your password must be between 4 and 20 characters long."
+            )
+        ]
+    )
+    confirm_password = PasswordField(
+        "Confirm Password",
+        validators=[
+            InputRequired(),
+            EqualTo(
+                "password",
+                message="This password did not match the one in the password field."
+            )
+        ]
+    )
+    submit = SubmitField("Register")
+
+
+class LoginForm(FlaskForm):
+    email =StringField("Email", validators=[InputRequired(), Email()])
+    password = PasswordField("Password", validators=[InputRequired()])
+    submit = SubmitField("Login")
